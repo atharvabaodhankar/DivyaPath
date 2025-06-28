9816e8d2ac836826cf27ef6df2f9282f9f211aad
AtharvaPC <baodhankaratharva@gmail.com>
2025-06-28 09:19:13 +0530

    fix: add missing closing brace in Aartis.jsx

--- src/pages/Aartis.jsx
+++ src/pages/Aartis.jsx
@@ -11,7 +11,7 @@ function Aartis() {
       .then(response => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`)
-        
+        }
         return response.json()
       })
       .then(data => {
