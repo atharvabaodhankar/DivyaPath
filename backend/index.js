816052d408ed6e7a3775d27f2010652175f2ebab
AtharvaPC <baodhankaratharva@gmail.com>
2025-06-29 08:31:43 +0530

    Added .env with CORS for security

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,10 @@ import aartiRoutes from './routes/aartis.js'
 import cors from 'cors'
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+  credentials: true,
+}))
 app.use(express.json())
 
 connectDB()
